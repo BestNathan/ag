4f7cfc43e27f2e10ac7f6d69bfcf13d68d909879
Nathan <308719298@qq.com>
2018-12-09 23:59:10 +0800

    优化。完成

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const LoginStore = require('./lib/store/loginStore');
-const PlazaStore = require('./lib/store/plazaStore');
-const BacStore = require('./lib/store/bacStore');
+const plazaService = require('./lib/service/plaza');
+const bacService = require('./lib/service/bac');
+const config = require('./lib/config');
+
+const {
+    HUB_GAME_BEGIN
+} = require('./lib/common/events');
 
 ~async function main() {
-    const login = new LoginStore();
-    const plaza = new PlazaStore();
-    const bac = new BacStore('C001');
-
+    await config.load();
+    const vid = 'C001';
     const userName = 'a308719298';
     const password = 'bd6277f1f882fae28ecf9463441ef66f';
 
-    const {loginName, token} = await login.startLogin(userName, password);
-    // await plaza.startLogin(loginName, token);
-    await bac.startLogin(loginName, token);
+    const plaza = await plazaService.plazaLogin(userName, password);
+    plaza.on(HUB_GAME_BEGIN, function onGameBegin(v) {
+        console.log(`vid: ${v} is begin to bet`);
+    })
 }()
